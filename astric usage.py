6e23bbc4c11c7abb4d0d38c6264cd0e6aa3b4740
Chirag Verma <cv091998@gmail.com>
2021-04-23 15:47:59 +0530

    Add files via upload

--- astric usage.py
+++ astric usage.py
@@ -13,3 +13,29 @@ beg, *middle, last = my_tuple
 print(beg)
 print(middle)
 print(last)
+print("\n#####\n")
+
+beg,second, *middle, second_last, last = my_tuple
+print(beg)
+print(second)
+print(middle)
+print(second_last)
+print(last)
+print("\n#####\n")
+
+
+my_list = [1, 2, 3]
+my_tuple = (4, 5, 6)
+my_set = {7, 8, 9}
+# my_set = {4, 5, 6}
+
+c_list = [*my_list, *my_tuple, *my_set]
+# c_list = [*my_list, *my_set]
+print(c_list)
+
+my_dict1 = {'a': 1, 'b': 2}
+my_dict2 = {'c': 3, 'd': 4}
+c_dict = {**my_dict1, **my_dict2}
+print(c_dict)
+
+
